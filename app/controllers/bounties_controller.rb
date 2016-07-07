e39be9f286cd65288efd0e63d292005844035ca4
Tobias Merkle <tgamerkle@gmail.com>
2016-07-07 14:19:54 -0700

    bounty form already... redy!

---
+++ app/controllers/bounties_controller.rb
@@ -0,0 +1,4 @@
+class BountiesController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'map/index'
+  resources :bounties
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'map#index'
 end
