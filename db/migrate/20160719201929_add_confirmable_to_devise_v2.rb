9f37db349ef12a6ccb06240d660956b5571c8ec7
Tobias Merkle <tgamerkle@gmail.com>
2016-07-19 13:36:50 -0700

    signin/out

---
+++ db/migrate/20160719201929_add_confirmable_to_devise_v2.rb
@@ -0,0 +1,7 @@
+class AddConfirmableToDeviseV2 < ActiveRecord::Migration[5.0]
+  def change
+    change_table(:users) do |t|
+       t.string   :unconfirmed_email # Only if using reconfirmable
+     end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719075240) do
+ActiveRecord::Schema.define(version: 20160719201929) do
 
   create_table "bounties", force: :cascade do |t|
     t.float    "lat"
@@ -40,6 +40,11 @@ ActiveRecord::Schema.define(version: 20160719075240) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
