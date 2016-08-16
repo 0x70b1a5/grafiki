d8929700db445b7cafb0b4aea6c3f4204a3ed6e2
grafiki-dev <go@grafiki.org>
2016-08-16 06:03:27 +0000

    fix migration fuckery

--- db/migrate/20160718222059_add_fields_to_users.rb
+++ db/migrate/20160718222059_add_fields_to_users.rb
@@ -1,8 +1,4 @@
 class AddFieldsToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :username, :string
-    add_column :users, :email, :string
-    add_column :users, :encrypted_password, :string
-    add_column :users, :salt, :string
   end
 end
