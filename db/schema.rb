04d728ed672fa3c78ee3fe83fcbbb736cd08d45f
Tobias Merkle <tgamerkle@gmail.com>
2016-08-16 14:28:39 -0700

    trying to add confirmable to users, pt i

--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 20160729072309) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
