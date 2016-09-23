2d8418fea72ad93f0074aed9b6e109bfaff0c69f
Tobias Merkle <tgamerkle@gmail.com>
2016-09-22 17:23:09 -0700

    try static service

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( spraycan.svg )
+Rails.application.config.serve_static_files = true
